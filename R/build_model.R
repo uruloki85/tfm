b50a917a29ff60418b05ca869d036e558f6c5616
uruloki85 <sabela85@gmail.com>
2021-05-18 18:13:47 +0200

    Update

--- R/build_model.R
+++ R/build_model.R
@@ -10,230 +10,136 @@ study <- cBioDataPack(
   ask = TRUE
 )
 
-study
-
-# Metadata
-
-colData(study)
-names(colData(study))
-
-table(study$OS_STATUS)
-# table(study$OS_MONTHS)
-table(study$DSS_STATUS)
-# table(study$DSS_MONTHS)
-table(study$DFS_STATUS)
-# table(study$DFS_MONTHS)
-table(study$PFS_STATUS)
-# table(study$PFS_MONTHS)
-table(study$AGE)
-table(study$SEX)
-
-# How many samples are present in each of the experiments
-library(UpSetR)
-upsetSamples(study)
-
-# Missing values
-missing_values <- data.frame(sum(is.na(study$OS_STATUS)))
-colnames(missing_values) <- "OS_STATUS"
-rownames(missing_values) <- "missing"
-missing_values$OS_MONTHS <- sum(is.na(study$OS_MONTHS))
-missing_values$DSS_STATUS <- sum(is.na(study$DSS_STATUS))
-missing_values$DSS_MONTHS <- sum(is.na(study$DSS_MONTHS))
-missing_values$DFS_STATUS <- sum(is.na(study$DFS_STATUS))
-missing_values$DFS_MONTHS <- sum(is.na(study$DFS_MONTHS))
-missing_values$PFS_STATUS <- sum(is.na(study$PFS_STATUS))
-missing_values$PFS_MONTHS <- sum(is.na(study$PFS_MONTHS))
-missing_values
-
-# Experiment data
-
-
-experiments(study)
-
-experiments(study[["RNA_Seq_v2_mRNA_median_all_sample_Zscores"]])
-assay <- assay(study[["RNA_Seq_v2_mRNA_median_all_sample_Zscores"]])
-assay
-# metadata(assay)
-
-# Get expression data
-eset <- study@ExperimentList@listData[["RNA_Seq_v2_mRNA_median_all_sample_Zscores"]]@assays@data@listData[[1]]
-
-
-metadata(study)
-
-
-##########################################
-#              Correlation               #
-##########################################
-
-# maemerge <- mergeReplicates(intersectColumns(study))
-# study_wide <- wideFormat(maemerge, colDataCols=c("OS_STATUS","PFS_STATUS"))
+####################
+# Prepare the data #
+####################
 
 subacc <- study[, , "RNA_Seq_v2_mRNA_median_all_sample_Zscores"]
 nrow(assay(subacc)) # 20531
 ncol(assay(subacc)) # 177
 # subacc <- intersectColumns(subacc)
 # subacc <- intersectRows(subacc)
+my_data <- wideFormat(subacc, colDataCols=c("PFS_MONTHS"), )
+# Drop column
+my_data$primary <- NULL
 
-subacc_wide <- wideFormat(subacc, colDataCols=c("OS_MONTHS","PFS_MONTHS"), )
-#subacc_wide_orig <- wideFormat(subacc, colDataCols=c("OS_MONTHS","PFS_MONTHS"), )
-
-nrow(subacc_wide) # 177
-ncol(subacc_wide) # 20534
-
-# rownames(subacc_wide)
-col_names <- colnames(subacc_wide)
-#length((col_names))
-
-# Remove the prefix with the experiment name in all the columns
-new_col_names <- sub("RNA_Seq_v2_mRNA_median_all_sample_Zscores_", "", col_names)
-#length((new_col_names))
-
-# Set the new column names
-colnames(subacc_wide) <- new_col_names
-
-# subacc_wide[0:3,0:5]
-# subacc_wide[0:3,20531:20534]
-# subacc_wide_orig[0:3,20531:20534]
-# subacc_wide[0:3,-1]
-
-# gene_signature_df <- read.csv(file = "signature_final.csv", header = TRUE)
-# gene_signature <- gene_signature_df[['x']]
 
 gene_signature_df <- read.csv(file = "signature_gene_expression_vs_outcome.csv", header = TRUE)
 gene_signature <- gene_signature_df[gene_signature_df$count >= 2, ][['HGNC']]
-
 length(gene_signature)
-genes_in_common <- intersect(gene_signature,colnames(subacc_wide))
-length(genes_in_common)
 
-cols_subset <- c(genes_in_common, c("OS_MONTHS","PFS_MONTHS"))
-length(cols_subset)
+my_data <- fix_col_names(my_data)
 
-# [,-1] to remove the first column which contains the sample name
-# mycors <- cor(as.matrix(subacc_wide[,-1]))
+# Find genes in common
+genes_in_common <- intersect(gene_signature,colnames(my_data))
+# length(genes_in_common)
+cols_subset <- c(genes_in_common, c("PFS_MONTHS"))
+# length(cols_subset)
 
-# subacc_wide[,cols_subset]
-mycors <- cor(as.matrix(subacc_wide[,cols_subset]))
-# mycors <- abs(mycors)
-diag(mycors) <- NA
+# Subset the data to only those columns of interest
+my_data <- my_data[,cols_subset]
 
-# show only components that have at least one correlation greater than 0.5.
-# has.high.cor <- apply(mycors, 2, max, na.rm=TRUE) > 0.5
-# mycors <- mycors[has.high.cor, has.high.cor]
-# pheatmap::pheatmap(mycors)
+min(my_data$PFS_MONTHS)
+max(my_data$PFS_MONTHS)
 
-mycors[lower.tri(mycors)] <- NA
-pheatmap::pheatmap(mycors, cluster_row =F, cluster_cols=F, na_col="white")
+# Histogram
+hist(my_data$PFS_MONTHS, main="Histogram", xlab="PFS_MONTHS", col="light green")
 
-library(reshape2)
-library(ggplot2)
+# Median
+median(my_data$PFS_MONTHS)
 
-# Get upper triangle of the correlation matrix
-get_upper_tri <- function(cormat){
-  cormat[lower.tri(cormat)]<- NA
-  return(cormat)
-}
+##################
+# Missing values #
+##################
 
-reorder_cormat <- function(cormat){
-  # Use correlation between variables as distance
-  dd <- as.dist((1-cormat)/2)
-  hc <- hclust(dd)
-  cormat <-cormat[hc$order, hc$order]
-}
+# Explore missing values
+sapply(my_data, function(x) sum(is.na(x)))
 
-mycors <- cor(as.matrix(subacc_wide[,cols_subset]))
+######################
+# Correlation matrix #
+######################
+
+#1st step
+# Correlation matrix with all genes
+my_cors2 <- cor(as.matrix(my_data))
+my_cors2_filtered <- my_cors2["PFS_MONTHS",]
+#abs(my_cors2_filtered) <= .90 & 
+index <- which(abs(my_cors2_filtered) >= .15, arr.ind = T)
+length(my_cors2_filtered[index])
+# my_cors2_filtered[index]
+filtered_gene_names <- names(my_cors2_filtered[index])
+# filtered_gene_names <- filtered_gene_names[-length(filtered_gene_names)]
+
+my_data <- my_data[,filtered_gene_names]
+
+# 2nd step
+# Correlation matrix with the genes selected in the previous step
+my_cors3 <- cor(as.matrix(my_data[,filtered_gene_names]))
 # Reorder the correlation matrix
-cormat <- reorder_cormat(mycors)
-upper_tri <- get_upper_tri(mycors)
+cormat3 <- reorder_cormat(my_cors3)
+cormat3[lower.tri(cormat3)] <- NA
 # Melt the correlation matrix
-melted_cormat <- melt(upper_tri, na.rm = TRUE)
-# Create a ggheatmap
-# ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value)) +
-#   geom_text(aes(Var2, Var1, label = value), color = "black", size = 1) +
-#   geom_tile(color = "white") +
-#   scale_fill_gradient2(low = "blue", high = "red", mid = "white",
-#                        midpoint = 0, limit = c(-1,1),
-#                        space = "Lab", name = "Pearson\ncorrelation") +
-#   # theme_minimal()+ 
-#   theme(
-#     axis.text.x = element_text(angle = 45, vjust = 1, 
-#                                size = 8, hjust = 1),
-#     axis.text.y = element_text(size = 8),
-#     axis.title.x = element_blank(),
-#     axis.title.y = element_blank(),
-#     panel.grid.major = element_blank(),
-#     panel.border = element_blank(),
-#     panel.background = element_blank(),
-#     axis.ticks = element_blank(),
-#     legend.justification = c(1, 0),
-#     legend.position = c(0.6, 0.7),
-#     legend.direction = "horizontal") +
-#   guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-#                                title.position = "top", title.hjust = 0.5))
-# # Print the heatmap
-# print(ggheatmap)
-
-
-ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
-  geom_tile(color = "white")+
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab", 
-                       name="Pearson\nCorrelation") +
-  theme_minimal()+ # minimal theme
-  coord_fixed() +
-  geom_text(aes(Var2, Var1, label = sprintf("%0.2f", round(value, digits = 2))), 
-            color = "black", 
-            size = 2) +
-  theme(
-    axis.text.x = element_text(angle = 45, vjust = 1, 
-                               size = 8, hjust = 1),
-    axis.text.y = element_text(size = 8),
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank(),
-    axis.ticks = element_blank(),
-    legend.justification = c(1, 0),
-    legend.position = c(0.6, 0.7),
-    legend.direction = "horizontal")+
-  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                               title.position = "top", title.hjust = 0.5))
-
+melted_cormat3 <- melt(cormat3, na.rm = TRUE)
+ggheatmap <- create_ggheatmap(melted_cormat3,"")
 print(ggheatmap)
-# # Get the assays
-# subacc.list <- assays(subacc)
-# # Transpose rows to columns, so genes are in columns
-# subacc.list <- lapply(subacc.list, t)
-# 
-# colnames(subacc.list[["RNA_Seq_v2_mRNA_median_all_sample_Zscores"]])
-# rownames(subacc.list[["RNA_Seq_v2_mRNA_median_all_sample_Zscores"]])
-# 
-# corres <- cor(subacc.list[[1]])
-# has.high.cor <- apply(corres, 2, max, na.rm=TRUE) > 0.5
-# mycors <- corres[has.high.cor, has.high.cor]
-# 
-# library(pheatmap)
-# pheatmap::pheatmap(mycors)
+
+# Discard the diagonal of the matrix
+diag(my_cors3) <- NA 
+# Check if there is any value higher than 0.9
+index <- which(abs(my_cors3) > .9, arr.ind = T) 
+if (index > 0) {
+  cbind.data.frame(gene1 = rownames(my_cors3)[index[,1]], # get the row name 
+                   gene2 = colnames(my_cors3)[index[,2]]) # get the column name
+  names(my_data[index])
+}
+
+###################################
+#               Model             #
+###################################
+library(randomForest)
+
+# my_data_saved <- my_data
+# my_data <- my_data_saved
+
+# Prepare the class variable using the median of PFS_MONTHS
+median_pfs <- median(my_data$PFS_MONTHS)
+my_data$class[my_data$PFS_MONTHS <= median_pfs] <- "0"
+my_data$class[my_data$PFS_MONTHS > median_pfs] <- "1"
+my_data$class <- as.factor(my_data$class)
+# Check new column
+my_data[,c("PFS_MONTHS","class")]
+# Remove PFS_MONTHS
+my_data$PFS_MONTHS <- NULL
+
+my_data$class
+
+my_matrix <- as.matrix(my_data)
+my_df <- as.data.frame(my_matrix)
+my_df$class <- as.factor(my_df$class)
+
+# Set random seed to make results reproducible:
+set.seed(42)
+
+# Calculate the size of each of the data sets:
+data_set_size <- floor(nrow(my_df) * .7)
+# Generate a random sample of "data_set_size" indexes
+indexes <- sample(1:nrow(my_df), size = data_set_size)
+
+# Assign the data to the correct sets
+training <- my_df[indexes,]
+validation1 <- my_df[-indexes,]
+nrow(training)
+nrow(validation1)
+
+rf_classifier <- randomForest(class ~ ., data=training, ntree=100, mtry=2)
+rf_classifier
+# varImpPlot(rf_classifier)
+
+#predicting the class for the test data set
+tree.pred1 <- predict(rf_classifier,validation1,type="class")
+
+#creating confusion matrix
+table(tree.pred1,validation1$class) 
 
 
 
-####################################################
-#           Kaplan-Meier Survival Analysis         #
-####################################################
-library(survival)
-library(survminer)
-
-# Surv(study$OS_MONTHS, as.numeric(as.factor(study$OS_STATUS)))
-
-# Remove patients missing survival information
-study_surv <- study[, complete.cases(study$OS_MONTHS, study$OS_STATUS), ]
-study_surv_df <- as.data.frame(colData(study_surv))
-
-# Convert OS_STATUS to numeric
-study_surv_df$OS_STATUS <- as.numeric(as.factor(study$OS_STATUS))
-
-# Survival analysis
-fit_surv <- survfit(Surv(OS_MONTHS, OS_STATUS) ~ PATH_N_STAGE, data = study_surv_df)
-ggsurvplot(fit_surv, data = colData(study_surv), risk.table = TRUE)
---
+++ R/explore_study.R
@@ -0,0 +1,189 @@
+library(cBioPortalData)
+# library(AnVIL)
+
+cbio <- cBioPortal()
+
+study <- cBioDataPack(
+  "paad_tcga_pan_can_atlas_2018",
+  use_cache = TRUE,
+  names.field = c("Hugo_Symbol", "Entrez_Gene_Id", "Gene"),
+  ask = TRUE
+)
+
+study
+
+# Metadata
+
+colData(study)
+names(colData(study))
+
+table(study$OS_STATUS)
+# table(study$OS_MONTHS)
+table(study$DSS_STATUS)
+# table(study$DSS_MONTHS)
+table(study$DFS_STATUS)
+# table(study$DFS_MONTHS)
+table(study$PFS_STATUS)
+# table(study$PFS_MONTHS)
+table(study$AGE)
+table(study$SEX)
+
+# How many samples are present in each of the experiments
+library(UpSetR)
+upsetSamples(study)
+
+# Explore missing values
+missing_values <- data.frame(sum(is.na(study$OS_STATUS)))
+colnames(missing_values) <- "OS_STATUS"
+rownames(missing_values) <- "missing"
+missing_values$OS_MONTHS <- sum(is.na(study$OS_MONTHS))
+missing_values$DSS_STATUS <- sum(is.na(study$DSS_STATUS))
+missing_values$DSS_MONTHS <- sum(is.na(study$DSS_MONTHS))
+missing_values$DFS_STATUS <- sum(is.na(study$DFS_STATUS))
+missing_values$DFS_MONTHS <- sum(is.na(study$DFS_MONTHS))
+missing_values$PFS_STATUS <- sum(is.na(study$PFS_STATUS))
+missing_values$PFS_MONTHS <- sum(is.na(study$PFS_MONTHS))
+missing_values
+
+####################################################
+#              Correlation Analysis                #
+####################################################
+
+subacc <- study[, , "RNA_Seq_v2_mRNA_median_all_sample_Zscores"]
+nrow(assay(subacc)) # 20531
+ncol(assay(subacc)) # 177
+# subacc <- intersectColumns(subacc)
+# subacc <- intersectRows(subacc)
+
+subacc_wide <- wideFormat(subacc, colDataCols=c("OS_MONTHS","PFS_MONTHS"), )
+nrow(subacc_wide) # 177
+ncol(subacc_wide) # 20534
+
+fix_col_names <- function(subacc) {
+  col_names <- colnames(subacc)
+  # Remove the prefix with the experiment name in all the columns
+  new_col_names <- sub("RNA_Seq_v2_mRNA_median_all_sample_Zscores_", "", col_names)
+  # Set the new column names
+  colnames(subacc) <- new_col_names
+  return(subacc)
+}
+
+subacc_wide <- fix_col_names(subacc_wide)
+# colnames(subacc_wide)
+
+subacc_wide_only_dead <- wideFormat(subacc, colDataCols=c("OS_MONTHS","PFS_MONTHS", "OS_STATUS"), )
+subacc_wide_only_dead <- subacc_wide_only_dead[subacc_wide_only_dead$OS_STATUS == "1:DECEASED",]
+# Drop column
+subacc_wide_only_dead$OS_STATUS <- NULL
+nrow(subacc_wide_only_dead) # 92
+ncol(subacc_wide_only_dead) # 20534
+
+subacc_wide_only_dead <- fix_col_names(subacc_wide_only_dead)
+# colnames(subacc_wide_only_dead)
+
+# subacc_wide[0:3,0:5]
+# subacc_wide[0:3,20531:20534]
+# subacc_wide_orig[0:3,20531:20534]
+# subacc_wide[0:3,-1]
+
+# gene_signature_df <- read.csv(file = "signature_final.csv", header = TRUE)
+# gene_signature <- gene_signature_df[['x']]
+
+# Get signature for "Gene expression vs outcome"
+gene_signature_df <- read.csv(file = "signature_gene_expression_vs_outcome.csv", header = TRUE)
+gene_signature <- gene_signature_df[gene_signature_df$count >= 2, ][['HGNC']]
+length(gene_signature) #28
+
+# Get signature for "Treatment vs outcome"
+
+
+# Get signature common to both groups
+
+
+
+library(reshape2)
+library(ggplot2)
+
+reorder_cormat <- function(cormat){
+  # Use correlation between variables as distance
+  dd <- as.dist((1-cormat)/2)
+  hc <- hclust(dd)
+  cormat <-cormat[hc$order, hc$order]
+  return (cormat)
+}
+
+create_ggheatmap <- function(metled_corr, title) {
+  ggheatmap <- ggplot(metled_corr, aes(Var2, Var1, fill = value))+
+    geom_tile(color = "white")+
+    scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                         midpoint = 0, limit = c(-1,1), space = "Lab", 
+                         name=paste(title,"Pearson","Correlation",sep="\n")) +
+    theme_minimal()+ # minimal theme
+    coord_fixed() +
+    geom_text(aes(Var2, Var1, label = sprintf("%0.2f", round(value, digits = 2))), 
+              color = "black", 
+              size = 2) +
+    theme(
+      axis.text.x = element_text(angle = 45, vjust = 1, 
+                                 size = 8, hjust = 1),
+      axis.text.y = element_text(size = 8),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.border = element_blank(),
+      panel.background = element_blank(),
+      axis.ticks = element_blank(),
+      legend.justification = c(1, 0),
+      legend.position = c(0.6, 0.7),
+      legend.direction = "horizontal")+
+    guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                                 title.position = "top", title.hjust = 0.5))
+  return (ggheatmap)
+}
+
+calculate_corr_and_heatmap <- function(subacc, title) {
+  # Find genes in common
+  genes_in_common <- intersect(gene_signature,colnames(subacc))
+  # length(genes_in_common)
+  cols_subset <- c(genes_in_common, c("OS_MONTHS","PFS_MONTHS"))
+  # length(cols_subset)
+  
+  mycors <- cor(as.matrix(subacc[,cols_subset]))
+  # Reorder the correlation matrix
+  cormat <- reorder_cormat(mycors)
+  # cormat <- mycors
+  cormat[lower.tri(cormat)] <- NA
+  # Melt the correlation matrix
+  melted_cormat <- melt(cormat, na.rm = TRUE)
+  ggheatmap <- create_ggheatmap(melted_cormat,title)
+  return (ggheatmap)
+}
+
+ggheatmap <- calculate_corr_and_heatmap(subacc_wide, 
+                                        "All patients")
+print(ggheatmap)
+ggheatmap_only_dead <- calculate_corr_and_heatmap(subacc_wide_only_dead, 
+                                                  "Only dead patients")
+print(ggheatmap_only_dead)
+
+
+
+
+####################################################
+#           Kaplan-Meier Survival Analysis         #
+####################################################
+library(survival)
+library(survminer)
+
+# Surv(study$OS_MONTHS, as.numeric(as.factor(study$OS_STATUS)))
+
+# Remove patients missing survival information
+study_surv <- study[, complete.cases(study$OS_MONTHS, study$OS_STATUS), ]
+study_surv_df <- as.data.frame(colData(study_surv))
+
+# Convert OS_STATUS to numeric
+study_surv_df$OS_STATUS <- as.numeric(as.factor(study$OS_STATUS))
+
+# Survival analysis
+fit_surv <- survfit(Surv(OS_MONTHS, OS_STATUS) ~ PATH_N_STAGE, data = study_surv_df)
+ggsurvplot(fit_surv, data = colData(study_surv), risk.table = TRUE)
